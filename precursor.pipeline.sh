95617526b0c5a6f4eea6bd70b565d00b13cd380b
jordan-brown-trestle <jordan.brown@trestlebiosciences.com>
2023-03-28 23:49:49 -0500

    added adaptor specification

--- precursor.pipeline.sh
+++ precursor.pipeline.sh
@@ -13,7 +13,6 @@ echo """#!/bin/bash
 mkdir $HOME/termination/$lib
 cd $HOME/termination/$lib
 cp $HOME/reference/WS230/ce_WS230.pirna.improve.fa ./
-cp $HOME/scripts/construct_precursor_df.R ./
 
 cp $HOME/results/$lib/$lib.inserts.gz ./
 gunzip $lib.inserts.gz
@@ -27,9 +26,10 @@ awk '{print \">\" \$1 \",\" \$5 \",\" \$3 \"\n\" \$3; }' $lib.pirna.bowtie > $li
 
 bowtie $HOME/reference/WS230/c_elegans.WS230.genomic $lib.bed.fa -p $bowtie_cores -v 0 --best --strata -a -f > $lib.precursor.v0.bowtie
 
-# bowtie $HOME/reference/WS230/c_elegans.WS230.genomic $lib.bed.fa -p 8 -v 1 --best --strata -a -f > $lib.precursor.v1.bowtie
+# bowtie $HOME/reference/WS230/c_elegans.WS230.genomic $lib.bed.fa -p $bowtie_cores -v 1 --best --strata -a -f > $lib.precursor.v1.bowtie
 
-Rscript construct_precursor_df.R $lib
+cd $HOME
+Rscript $HOME/scripts/construct_precursor_df.R $lib $bowtie_cores
 """>$sgefile
 
 echo $sgefile
